0634c239059fa447df8f3cd57dc68790e94a60af
philipp <philipp.gaag@HS-Augsburg.de>
2021-12-01 21:01:21 +0100

    Initial commit

---
+++ door_one/target/debug/deps/door_one-f5981ba8be63d6d7.d
@@ -0,0 +1,5 @@
+/home/philipp/adventofcode/door_one/target/debug/deps/door_one-f5981ba8be63d6d7: src/main.rs
+
+/home/philipp/adventofcode/door_one/target/debug/deps/door_one-f5981ba8be63d6d7.d: src/main.rs
+
+src/main.rs:
---
+++ door_one/target/debug/door_one.d
@@ -0,0 +1,1 @@
+/home/philipp/adventofcode/door_one/target/debug/door_one: /home/philipp/adventofcode/door_one/src/main.rs
